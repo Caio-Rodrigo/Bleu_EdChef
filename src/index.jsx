759aaffe86c6960cce27e5ebc73da4e427b99438
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-08-09 20:47:55 -0300

    nova rota

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Navigation from './components/Navigation/index';
 import './index.css';
-import Home from './components/Home/index.jsx';
+import Home from './components/views/Home/index.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
